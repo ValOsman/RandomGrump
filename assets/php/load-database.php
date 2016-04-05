a93d83d2dba8703e25ba0d765840508da130a379
Jon Osman <jonjosman@gmail.com>
2016-04-05 13:32:41 -0700

    Added one-offs and sorting
    Added filtering results by one-offs and added sorting buttons to the
    watch page.

--- assets/php/load-database.php
+++ assets/php/load-database.php
@@ -1,14 +1,27 @@
 <?php
-    ob_start();
     set_time_limit(0);
     $dbStartTime = microtime(true);
     echo("Started loading database... <br>");
-    require('yt-key.php');
+    $channelName = "gamegrumps";
+    $MAX_RESULTS = 50;
+
+    //Get channel ID and uploads playlist
+    require('yt-key.php');  
+    $request = 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=' . $channelName . '&key='. MY_KEY;
+    $channel = json_decode(file_get_contents($request), true);
+    $channelId = $channel["items"][0]["id"];
+    $uploadsId = $channel["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"];
+      
     require('load-videos.php');
+
+    $selectQuery = "SELECT * FROM video";
+    $stmt = $dbh->prepare($selectQuery);
+    $stmt->execute();
+    print_r($stmt->fetch());
+
     require('load-playlists.php');
     require('videos-in-playlists.php');
 
     $dbEndTime = microtime(true);
     echo("Finished loading database. Total runtime: " . date("i:s",$dbEndTime-$dbStartTime) . "<br>");
-    ob_end_flush();
 ?>
--- assets/php/load-playlists.php
+++ assets/php/load-playlists.php
@@ -1,26 +1,14 @@
 <?php
-ini_set('max_execution_time', 300);
-$channelName = "gamegrumps";
-require_once('yt-key.php');
-$MAX_RESULTS = 50;
 $playlistStartTime = microtime(true);
 
 echo("Started loading playlists... <br>");
 
-$request = 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=' . $channelName . '&key='. MY_KEY;
-$channel = json_decode(file_get_contents($request), true);
-$channelId = $channel["items"][0]["id"];
-$uploadsId = $channel["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"];
-//print_r($uploadsId);
-//echo($request);
-//echo("<br><br>");
+//Get first page of playlists and then iterate over remaining pages.
 $request = 'https://www.googleapis.com/youtube/v3/playlists?part=contentDetails,snippet&channelId=' . $channelId . '&maxResults=' . $MAX_RESULTS . '&key='. MY_KEY;
 $playlistsResults = json_decode(file_get_contents($request), true);
 $nextPageToken = $playlistsResults["nextPageToken"];
 $playlists = $playlistsResults["items"];
-//print_r($playlists[0]);
-//echo($request);
-//echo("<br><br>");
+
 while ($nextPageToken != "") {
     $request = 'https://www.googleapis.com/youtube/v3/playlists?part=contentDetails,snippet&channelId=' . $channelId . '&maxResults=' . $MAX_RESULTS . '&pageToken='. $nextPageToken . '&key='. MY_KEY;
     $playlistsResults = json_decode(file_get_contents($request), true);
@@ -31,16 +19,22 @@ while ($nextPageToken != "") {
     }
     $playlists = array_merge($playlists, $playlistsResults["items"]);
 }
-//print_r($playlists);
+
 $playerIdArray = array();
+
+//Format relevant playlist info into arrays and store those arrays in $playlistsFormatted.
 for ($i = 0; $i < sizeof($playlists); $i++) {
     $items = $playlists[$i];
     $snippet = $playlists[$i]["snippet"];
     $title = explode(" - ", $snippet["title"])[0];
     $length = $items["contentDetails"]["itemCount"];
-    $playlistsFormatted[] = array($items["id"], $title, $snippet["publishedAt"], $length);
+    $publishedAt = $snippet["publishedAt"];
+    $playlistsFormatted[] = array($items["id"], $title, $publishedAt, $length);
 }
 
+$playlistsFormatted[] = array($uploadsId, 'Uploads', NULL, NULL);
+
+//Connect to database, drop table if testing, create table if necessary
 try {
     $dbh = new PDO("sqlite:../db/randomgrumps.db");
 }
@@ -59,15 +53,15 @@ $query = "CREATE TABLE IF NOT EXISTS `playlist` (
     )";
 $dbh->exec($query);
 
-
+//Store data
 $dbh->beginTransaction();
 foreach ($playlistsFormatted as $playlist=>$array) {
-    $playlist_id = $dbh->quote($array[0]);
-    $title = $dbh->quote($array[1]);
-    $published_date = $dbh->quote($array[2]);
-    $episodes = $dbh->quote($array[3]);
-    $query = "INSERT OR IGNORE INTO playlist (playlist_id, title, published_date, episodes) VALUES ($playlist_id, $title, $published_date, $episodes)";
-    $dbh->query($query);
+    $stmt = $dbh->prepare("INSERT OR IGNORE INTO playlist (playlist_id, title, published_date, episodes) VALUES (:playlist_id, :title, :published_date, :episodes)");
+    $stmt->bindParam(":playlist_id", $array[0]);
+    $stmt->bindParam(":title", $array[1]);
+    $stmt->bindParam(":published_date", $array[2]);
+    $stmt->bindParam(":episodes", $array[3]);
+    $stmt->execute();
 }
 
 $dbh->commit();
